d1a1b7dbdafc3591a13796ec893295cb59fbe986
minishop <minishop@shopes.cn>
2017-10-10 04:18:51 +0800

    2017-10-09 20:18:51

--- public/plugins/material-wp/paradox/views/errors.php
+++
@@ -1,7 +0,0 @@
-<div class="error">
-  <p><?php printf(__('%s got some errors while processing your request:', $this->td), $this->config['name']); ?><br>
-    <?php foreach ($this->errors as $error) : ?>
-      <strong>· <?php echo $error; ?></strong><br>
-    <?php endforeach; ?>
-  </p>
-</div>
