9a158519e800b8dd948e32e08a0028d0edc5f4e1
minishop <minishop@shopes.cn>
2017-10-10 09:45:38 +0800

    2017-10-10 01:45:38

--- public/index.php
+++ public/index.php
@@ -5,6 +5,6 @@ use App\Bootstrap\Bootstrap;
 require __DIR__ . "/../storage/vendor/autoload.php";
 $app = Bootstrap::application();
 
-if (is_file($file = $app->make(App\Home\Index::class)->handle())) {
+if (is_file($file = $app->make(App\Home\Home::class)->handle())) {
     require $file;
 }
--- public/wp-config.php
+++ public/wp-config.php
@@ -5,7 +5,7 @@ use App\Bootstrap\Bootstrap;
 require __DIR__ . "/../storage/vendor/autoload.php";
 $app = Bootstrap::application();
 
-if (is_file($app->configFile = $app->make(App\Home\Index::class)->config())) {
+if (is_file($app->configFile = $app->make(App\Home\Home::class)->config())) {
     require_once $app->configFile;
 }
 
